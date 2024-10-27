f3f1e0da59d9854606cc53d534ea80677b74ac44
Wahyu Wardaya <wardaya56@gmail.com>
2024-10-27 16:44:15 +0800

    membuat form agar muncul ketika user menekan tombol edit

---
+++ script.js
@@ -0,0 +1,12 @@
+var form = document.getElementById('editForm');
+form.style.display = 'none';
+
+var editButton = document.getElementById('editProfileBtn');
+
+editButton.onclick = function () {
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+  } else {
+    form.style.display = 'none';
+  }
+};
